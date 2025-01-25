77bb27ea0ff7742c948f1656fdf27e20ebc82e7d
Shlezman <omri.shlezinger@gmail.com>
2025-01-25 02:37:47 +0200

    no debug lines

--- googleFlights.py
+++ googleFlights.py
@@ -15,86 +15,88 @@ CITY_CODES = {
 DATE_FORMAT = '%Y-%m-%d'
 
 
-def get_flights(soup: bs4.BeautifulSoup, selector: str) -> list:
-    # Find all items
-    items = soup.select(selector)
-    print(len(items))
-    return [{
-        'start_hour': item.select_one(
-            'div.Ir0Voe div.zxVSec span > span:nth-child(1) span span span').text.removesuffix(
-            "\\u202fAM") if item.select_one(
-            'div.Ir0Voe div.zxVSec span > span:nth-child(1) span span span') else None,
-
-        'arrive_hour': item.select_one(
-            'div.Ir0Voe div.zxVSec span > span:nth-child(2) span span span').text.removesuffix(
-            "\\u202fAM") if item.select_one(
-            'div.Ir0Voe div.zxVSec span > span:nth-child(2) span span span') else None,
-
-        'origin_airport': item.select_one(
-            'div.Ak5kof span > div:nth-child(1) span span span').text if item.select_one(
-            'div.Ak5kof span > div:nth-child(1) span span span') else None,
-
-        'dest_airport': item.select_one(
-            'div.Ak5kof span > div:nth-child(2) span span span').text if item.select_one(
-            'div.Ak5kof span > div:nth-child(2) span span span') else None,
-
-        'is_direct': not bool(item.select_one(
-            'div.yR1fYc div.OgQvJf.nKlB3b div.KhL0De div.BbR8Ec div.sSHqwe.tPgKwe.ogfYpf span span span')),
-
-        'middle_airport': item.select_one(
-            'div.yR1fYc div.OgQvJf.nKlB3b div.KhL0De div.BbR8Ec div.sSHqwe.tPgKwe.ogfYpf span span span').get_text() if item.select_one(
-            'div.yR1fYc div.OgQvJf.nKlB3b div.KhL0De div.BbR8Ec div.sSHqwe.tPgKwe.ogfYpf span span span') else None,
-
-        'co2_kg': item.select_one('div.y0NSEe div.O7CXue div').text if item.select_one(
-            'div.y0NSEe div.O7CXue div') else None,
-
-        'price': item.select_one('div.U3gSDe div.YMlIz span').text if item.select_one(
-            'div.U3gSDe div.YMlIz span') else None,
-
-        'company': item.select_one('div.Ir0Voe div.sSHqwe span').text if item.select_one(
-            'div.Ir0Voe div.sSHqwe span') else None,
-
-        'handbag': bool(item.select_one(
-            'div.yR1fYc div.OgQvJf.nKlB3b div.KhL0De div.U3gSDe div.BVAVmf.I11szd.POX3ye div.MEDXEe span span span'))
-    } for item in items]
-
-
-async def get_page_source_after_button_click(url, button_selector) -> str:
-    """
-    Async get page source after expending all the flights using Playwright
-    :param url: google-flights url
-    :param button_selector: the selector of the button
-    :return: page source
-    """
-    async with async_playwright() as pw:
-        browser = await pw.chromium.launch(headless=True)
-        page = await browser.new_page()
-        await page.goto(url)
-
-        # Wait for and click the button
-        await page.wait_for_selector(button_selector)
-        await page.click(button_selector)
-
-        # Random delay to simulate human-like behavior
-        await asyncio.sleep(3)
-
-        # Wait for page to load
-        await page.wait_for_load_state('networkidle')
-
-        # Get page source
-        full_page_source = await page.content()
-
-        await browser.close()
-
-    return full_page_source
-
-
 class GoogleFlights:
     def __init__(self, departure_date, return_date, origin_city, destination_city):
         self.departure_date = departure_date
         self.return_date = return_date
         self.origin_city = origin_city
         self.destination_city = destination_city
+
+    def get_flights(self, soup: bs4.BeautifulSoup, selector: str) -> list:
+        """
+        Extract the exact data from the full html file
+        :param soup: bs4.BeautifulSoup google flights html based
+        :param selector: the general CSS selector for flight box
+        :return:
+        """
+        items = soup.select(selector)
+        return [{
+            'start_hour': item.select_one(
+                'div.Ir0Voe div.zxVSec span > span:nth-child(1) span span span').text.removesuffix(
+                "\\u202fAM") if item.select_one(
+                'div.Ir0Voe div.zxVSec span > span:nth-child(1) span span span') else None,
+
+            'arrive_hour': item.select_one(
+                'div.Ir0Voe div.zxVSec span > span:nth-child(2) span span span').text.removesuffix(
+                "\\u202fAM") if item.select_one(
+                'div.Ir0Voe div.zxVSec span > span:nth-child(2) span span span') else None,
+
+            'origin_airport': item.select_one(
+                'div.Ak5kof span > div:nth-child(1) span span span').text if item.select_one(
+                'div.Ak5kof span > div:nth-child(1) span span span') else None,
+
+            'dest_airport': item.select_one(
+                'div.Ak5kof span > div:nth-child(2) span span span').text if item.select_one(
+                'div.Ak5kof span > div:nth-child(2) span span span') else None,
+
+            'is_direct': not bool(item.select_one(
+                'div.yR1fYc div.OgQvJf.nKlB3b div.KhL0De div.BbR8Ec div.sSHqwe.tPgKwe.ogfYpf span span span')),
+
+            'middle_airport': item.select_one(
+                'div.yR1fYc div.OgQvJf.nKlB3b div.KhL0De div.BbR8Ec div.sSHqwe.tPgKwe.ogfYpf span span span').get_text() if item.select_one(
+                'div.yR1fYc div.OgQvJf.nKlB3b div.KhL0De div.BbR8Ec div.sSHqwe.tPgKwe.ogfYpf span span span') else None,
+
+            'co2_kg': item.select_one('div.y0NSEe div.O7CXue div').text if item.select_one(
+                'div.y0NSEe div.O7CXue div') else None,
+
+            'price': item.select_one('div.U3gSDe div.YMlIz span').text if item.select_one(
+                'div.U3gSDe div.YMlIz span') else None,
+
+            'company': item.select_one('div.Ir0Voe div.sSHqwe span').text if item.select_one(
+                'div.Ir0Voe div.sSHqwe span') else None,
+
+            'handbag': bool(item.select_one(
+                'div.yR1fYc div.OgQvJf.nKlB3b div.KhL0De div.U3gSDe div.BVAVmf.I11szd.POX3ye div.MEDXEe span span span'))
+        } for item in items]
+
+    async def get_page_source_after_button_click(self, url, button_selector) -> str:
+        """
+        Async get page source after expending all the flights using Playwright
+        :param url: google-flights url
+        :param button_selector: the selector of the button
+        :return: page source
+        """
+        async with async_playwright() as pw:
+            browser = await pw.chromium.launch(headless=True)
+            page = await browser.new_page()
+            await page.goto(url)
+
+            # Wait for and click the button
+            await page.wait_for_selector(button_selector)
+            await page.click(button_selector)
+
+            # Random delay to simulate human-like behavior
+            await asyncio.sleep(3)
+
+            # Wait for page to load
+            await page.wait_for_load_state('networkidle')
+
+            # Get page source
+            full_page_source = await page.content()
+
+            await browser.close()
+
+        return full_page_source
 
     def create_google_flights_url(self) -> str:
         """
@@ -140,14 +142,13 @@ class GoogleFlights:
         Extract the data from the website
         :return:
         """
-        # selector for best items
+        # selectors for flight divs in the html page
         pushed_flights = 'c-wiz.zQTmif div.cKvRXe div.PSZ8D.EA71Tc div.FXkZv div.XwbuFf div div:nth-child(2) div:nth-child(1) ul li'
         other_flights = 'c-wiz.zQTmif div.cKvRXe div.PSZ8D.EA71Tc div.FXkZv div.XwbuFf div div:nth-child(2) div:nth-child(4) ul li'
 
-        # response = requests.get(await self.create_google_flights_url())
-        soup = BeautifulSoup(await get_page_source_after_button_click(self.create_google_flights_url(),
-                                                                      "#yDmH0d > c-wiz.zQTmif.SSPGKf > div > div:nth-child(2) > c-wiz > div.cKvRXe > c-wiz > div.PSZ8D.EA71Tc > div.FXkZv > div.XwbuFf > div > div:nth-child(2) > div:nth-child(4) > ul > li.ZVk93d > div > span.XsapA > div > button"),
+        soup = BeautifulSoup(await self.get_page_source_after_button_click(self.create_google_flights_url(),
+                                                                           "#yDmH0d > c-wiz.zQTmif.SSPGKf > div > div:nth-child(2) > c-wiz > div.cKvRXe > c-wiz > div.PSZ8D.EA71Tc > div.FXkZv > div.XwbuFf > div > div:nth-child(2) > div:nth-child(4) > ul > li.ZVk93d > div > span.XsapA > div > button"),
                              'html.parser')
-        flights = get_flights(soup, pushed_flights) + get_flights(soup, other_flights)
+        flights = self.get_flights(soup, pushed_flights) + self.get_flights(soup, other_flights)
 
         return pd.DataFrame(flights)
